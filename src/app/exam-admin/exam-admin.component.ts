8a451938d76704e29f218ffc2a205227ae1952f0
Rohit Tiwari <kacs.rohit.1994@gmail.com>
2020-05-11 00:25:44 +0530

    Import css issue fixed

--- src/app/exam-admin/exam-admin.component.ts
+++ src/app/exam-admin/exam-admin.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fn } from '@angular/compiler/src/output/output_ast';
+
+// declare var require: any
+declare var jQuery: any;
 
 @Component({
   selector: 'app-exam-admin',
   templateUrl: './exam-admin.component.html',
-  styleUrls: ['./exam-admin.component.css']
+  styleUrls: ['./exam-admin.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ExamAdminComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
