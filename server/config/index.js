8f2eb9519b89f4bab3fbdb959debf9146b3b6316
prajapati55 <prvndrkumar55@gmail.com>
2020-06-06 12:20:23 +0530

    initail commit for server and db

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongodb: {
+      url: 'mongodb://localhost:27017',
+      databaseName: 'online_examination',
+    },
+    migrationsDir: './migrations',
+    changelogCollectionName: 'changelog',
+  }
