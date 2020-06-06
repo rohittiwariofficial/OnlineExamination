67170a3346f65f7a3b505a778ceace169068ee62
prajapati55 <prvndrkumar55@gmail.com>
2020-06-06 12:24:43 +0530

    Revert "initail commit for server and db"
    This reverts commit 8f2eb9519b89f4bab3fbdb959debf9146b3b6316.

--- server/config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongodb: {
-      url: 'mongodb://localhost:27017',
-      databaseName: 'online_examination',
-    },
-    migrationsDir: './migrations',
-    changelogCollectionName: 'changelog',
-  }
